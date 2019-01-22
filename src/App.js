96f2c9b9276a46f2bf929430a686d726878402c3
JcCano <doblec92@hotmail.com>
2019-01-22 02:02:22 -0600

    Se agregaron archivos nuevos

--- src/App.js
+++ src/App.js
@@ -2,24 +2,58 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Navegacion from './componentes/navegacion';
+
+import {todos} from './tareas';
+
+console.log(todos);
+
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state ={
+      todos:todos
+    }
+
+  }
+
   render() {
+
+    const todos = this.state.todos.map((todo, i)=>{
+      return(
+          <div className="col-md-4">
+            <div className="card mt-4">
+              <div className="card-header">
+                <h3>{todo.titulo}</h3>
+                <span className="badge badge-pill badge-danger ml-2">
+                  {todo.prioridad}
+                </span>
+              </div>
+              <div className="card-body">
+                <p>{todo.descripcion}</p>
+                <p>{todo.responsable}</p>
+              </div>
+            </div>
+          </div>
+      )
+    })
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        {/* Se inserto la navegacion */}
+        <Navegacion/>
+        <hr/>
+
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <div className="container">
+          <div className="row mt-4">
+            {todos}
+          </div>
+        </div>
+        
       </div>
     );
   }
